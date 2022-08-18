29e9c7936ac6a7b5775ce3549a5387629828405c
Chiseeh <111065478+Chiseeh@users.noreply.github.com>
2022-08-17 20:33:53 -0400

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,6 @@
+num1 = input(float)
+num2 = input(float)
+
+Suma = num1+num2
+
+print("Operacion realizada ," + Suma)
---
+++ mul.py
@@ -0,0 +1,6 @@
+num1 = input(float)
+num2 = input(float)
+
+Multi = num1*num2
+
+print("Operacion realizada ," + Multi)
