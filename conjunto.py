ec4c4495b001154f9c605767542a0e80d4683923
Chiseeh <bas.valenzuelar@duocuc.cl>
2022-08-17 20:49:17 -0400

    Commit local 1

---
+++ conjunto.py
@@ -0,0 +1,9 @@
+num1 = input(float)
+num2 = input(float)
+
+Suma = num1+num2
+Resta = num1+num2
+Multi = num1*num2
+Div = (num1/num2)
+
+print("Operaciones realizadas con exito ," , "Suma"+ Suma, "Resta", + Resta, "Multiplicacion", + Multi, "Division", + Div)
---
+++ div.py
@@ -0,0 +1,6 @@
+num1 = input(float)
+num2 = input(float)
+
+Div = (num1/num2)
+
+print("Operacion realizada ," + Div)
